9fb92cf40792b496da42b01ac83da1cac1cd7845
felipe rocha <felipemarcelorocha1996@gmail.com>
2020-09-28 15:37:59 -0300

    Add project files.

---
+++ AssetsManagement/Data/AssetsContext.cs
@@ -0,0 +1,21 @@
+﻿using AssetsManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetsManagement.Data
+{
+    public class AssetsContext : DbContext
+    {
+        public DbSet<Assets> Assets { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=AssetsManagement;Data Source=DESKTOP-I2AF9TL");
+        }
+    }
+}
